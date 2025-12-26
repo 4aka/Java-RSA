b8f55d491d27bcf6dbcd843cac7c612ec2b28443
4aka <7nuclear@gmail.com>
2025-12-26 13:23:15 +0200

    Update main with examples

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,37 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
 
+    // https://www.youtube.com/watch?v=xqPgE-hPIfE
+
     public static void main(String[] args) {
-        int P = 3; int Q = 7;
+        int P = 13; int Q = 29; List<Integer> ciphered = new ArrayList<>();
 
         PublicKeyBuilder pbKeyBuilder = new PublicKeyBuilder(P, Q);
-        Map<String, Integer> pk = pbKeyBuilder.getPublicKey();
+        Map<common, Integer> publicKey = pbKeyBuilder.getPublicKey();
 
-        System.out.println("Public key Mod: " + pk.get("Mod"));
-        System.out.println("Public key Exp: " + pk.get("Exp"));
+        PrivateKeyBuilder prKeyBuilder = new PrivateKeyBuilder(P, Q, publicKey.get(common.EXP));
+        Map<common, Integer> privateKey = prKeyBuilder.getPrivateKey();
 
-        PrivateKeyBuilder prKeyBuilder = new PrivateKeyBuilder(P, Q, pk.get("Exp"));
-        Map<String, Integer> prk = prKeyBuilder.getPrivateKey();
+        print("Public key : " + publicKey);
+        print("Private key : " + privateKey);
 
-        System.out.println("Private key Mod: " + prk.get("Mod"));
-        System.out.println("Private key D: " + prk.get("D"));
+        //            a   r  c  h  i   n  c   o   r   p
+        int[] word = {1, 18, 3, 8, 9, 14, 3, 15, 18, 16};
+        print("Data: 1, 18, 3, 8, 9, 14, 3, 15, 18, 16");
+
+        for (int l: word) ciphered.add(new Ciphering(l, publicKey).cipher());
+        print("Ciphered word: " + ciphered);
+
+        List<BigInteger> deciphered = new Deciphering(privateKey).decipher(ciphered);
+        print("Deciphered word: " + deciphered);
+    }
+
+    public static void print(Object data) {
+        System.out.println(data);
     }
 }
